1065a27f5c28a02f2c712305b35bcab1c06b3d70
hermelawesene0924 <yabsirawesene920.com>
2026-06-16 01:33:41 +0300

    feat: add evaluate_calibration and plot_reliability_diagram to evaluation exports

--- bpc/training/evaluation.py
+++ bpc/training/evaluation.py
@@ -3,4 +3,4 @@
 from bpc.layers.predictive_network import predict
 from bpc.training.metrics import classification_accuracy, per_class_accuracy
 
-__all__ = ["classification_accuracy", "per_class_accuracy", "predict"]
+__all__ = ["classification_accuracy", "per_class_accuracy", "predict" "evaluate_calibration", "plot_reliability_diagram"]
